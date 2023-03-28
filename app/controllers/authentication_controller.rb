8134eba4cc7ff4446dfcd4dda2706772ace7a489
Desor Guishny <desorguishny@gmail.com>
2023-03-28 16:41:40 -0400

    Add login method to authentication controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,15 @@
 class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_user
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                     user_name: @user.user_name }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+
 end
