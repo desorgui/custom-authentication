b3491eaf5cac93641e80b1fa6845de041da4d74d
Desor Guishny <desorguishny@gmail.com>
2023-03-28 12:58:15 -0400

    Add Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+
+  skip_before_action :authenticate_user, only: [:create]
+  before_action :find_user, only [:show, :update, :destroy]
+
+  def index
+    @users = User.all
+    respond_to do |format|
+      format.html { render :index }
+    end
+  end
+
+  def show
+    @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :update }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
